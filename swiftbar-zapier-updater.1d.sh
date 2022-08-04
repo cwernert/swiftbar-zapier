3d05fce51fca74c47c0b387a6a9409e33c44157c
cwernert <55778578+cwernert@users.noreply.github.com>
2022-08-04 15:14:22 +1000

    Update swiftbar-zapier-updater.1d.sh

--- swiftbar-zapier-updater.1d.sh
+++ swiftbar-zapier-updater.1d.sh
@@ -3,24 +3,9 @@
 # <bitbar.version>v0.0.1</bitbar.version>
 # <bitbar.author>Christian Wernert</bitbar.author>
 # <bitbar.author.github>cwernert</bitbar.author.github>
-# <bitbar.dependencies>javascript, node (fetch, fs)</bitbar.dependencies>
-# <bitbar.desc>Displays contents set by a Zapier channel</bitbar.desc>
+# <bitbar.dependencies>bash</bitbar.dependencies>
+# <bitbar.desc>Checks for updates to the swiftbar-zapier plugin.</bitbar.desc>
 # <bitbar.abouturl>https://github.com/cwernert/swiftbar-zapier</bitbar.abouturl>
-# <swiftbar.hideAbout>false</swiftbar.hideAbout>
-# <swiftbar.hideRunInTerminal>true</swiftbar.hideRunInTerminal>
-# <swiftbar.hideLastUpdated>true</swiftbar.hideLastUpdated>
-# <swiftbar.hideDisablePlugin>true</swiftbar.hideDisablePlugin>
-# <swiftbar.hideSwiftBar>false</swiftbar.hideSwiftBar>
-
-
-# <xbar.title>SwiftBar for Zapier: Updater</xbar.title>
-# <xbar.version>v0.1</xbar.version>
-# <xbar.author>Christian Wernert</xbar.author>
-# <xbar.author.github>cwernert</xbar.author.github>
-# <xbar.desc>Checks for updates to the swiftbar-zapier plugin.</xbar.desc>
-# <xbar.image>https://upload.wikimedia.org/wikipedia/commons/f/fd/Zapier_logo.svg</xbar.image>
-# <xbar.dependencies>zsh</xbar.dependencies>
-# <xbar.abouturl>https://github.com/cwernert/swiftbar-zapier</xbar.abouturl>
 # <swiftbar.hideAbout>true</swiftbar.hideAbout>
 # <swiftbar.hideRunInTerminal>true</swiftbar.hideRunInTerminal>
 # <swiftbar.hideLastUpdated>true</swiftbar.hideLastUpdated>
@@ -31,7 +16,7 @@
 # Probably need to check version numbers, & if GitHub has a later version then invoke a separate shell script which isn't a SwiftBar plugin
 # That way, it can pkill -x SwiftBar, curl the files, chflags hide them all, then open -a SwiftBar again
 
-cd $SWIFTBAR_PLUGINS_PATH
+cd $SWIFTBAR_PLUGINS_PATH/swiftbar-zapier
 curl -O https://raw.githubusercontent.com/cwernert/swiftbar-zapier/main/swiftbar-zapier.10s.js
 curl -O https://raw.githubusercontent.com/cwernert/swiftbar-zapier/main/swiftbar-zapier-updater.1d.sh
 curl -O https://raw.githubusercontent.com/cwernert/swiftbar-zapier/main/package.json
