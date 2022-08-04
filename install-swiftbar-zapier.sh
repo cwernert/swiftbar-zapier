9b83fb0562cf63bd1f1ea14b53441b5534abf78a
cwernert <55778578+cwernert@users.noreply.github.com>
2022-08-04 13:53:31 +1000

    Update install-swiftbar-zapier.sh

--- install-swiftbar-zapier.sh
+++ install-swiftbar-zapier.sh
@@ -1,9 +1,32 @@
 #!/usr/bin/env bash
-echo "Creating SwiftBar Plugins directory..."
-mkdir -p ~/Applications/SwiftBar-Plugins
-cd ~/Applications/SwiftBar-Plugins
-echo "Downloading the swiftbar-zapier updater..."
+clear
+echo "This script will install swiftbar-zapier on your computer."
+echo "If SwiftBar is currently running, it will be closed now and opened again once installation is complete."
+while true; do
+  read -p "Are you ready to continue? (y/n)"
+  case $yn in
+    [yY] ) echo "Installing swiftbar-zapier..."
+            break;;
+    [nN] ) echo "Exiting. Installation aborted."
+            exit;;
+    * ) echo "Invalid response. Please enter Y or N.";;
+  esac
+done
+pkill -x SwiftBar
+echo "Creating swiftbar-zapier directory..."
+mkdir -p ~/Applications/SwiftBar-Plugins/swiftbar-zapier
+cd ~/Applications/SwiftBar-Plugins/swiftbar-zapier
+echo "Downloading swiftbar-zapier resources..."
+curl -O https://raw.githubusercontent.com/cwernert/swiftbar-zapier/main/swiftbar-zapier.10s.js
 curl -O https://raw.githubusercontent.com/cwernert/swiftbar-zapier/main/swiftbar-zapier-updater.1d.sh
+curl -O https://raw.githubusercontent.com/cwernert/swiftbar-zapier/main/package.json
+echo "Creating config directory..."
+mkdir config
+cd config
+echo "Downloading swiftbar-zapier-config resources..."
+curl -O https://raw.githubusercontent.com/cwernert/swiftbar-zapier/main/swiftbar-zapier-config.sh
+curl -O https://raw.githubusercontent.com/cwernert/swiftbar-zapier/main/swiftbar-zapier-config.js
+curl -O https://raw.githubusercontent.com/cwernert/swiftbar-zapier/main/swiftbar-zapier-config.json
 echo "Checking availability of dependencies..."
 installed="Installed:"
 required="Required:"
@@ -64,22 +87,25 @@ if [[ $reqCount > 0 ]] ; then
               exit;;
       * ) echo "Invalid response. Please enter Y or N.";;
     esac
-    if echo $required | grep -q 'xcode-select'; then
-      xcode-select --install
-    fi
-    if echo $required | grep -q 'homebrew'; then
-      /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"  
-      brew update
-    fi
-    if echo $required | grep -q 'node\|npm'; then
-      brew install node
-    fi
-    if echo $required | grep -q 'swiftbar'; then
-      brew install swiftbar
-    fi
   done
+  if echo $required | grep -q 'xcode-select'; then
+    xcode-select --install
+  fi
+  if echo $required | grep -q 'homebrew'; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+    brew update
+  fi
+  if echo $required | grep -q 'node\|npm'; then
+    brew install node
+  fi
+  if echo $required | grep -q 'swiftbar'; then
+    brew install swiftbar
+  fi
 else
   echo "All dependencies are installed."
   echo $installed
 fi
-echo "Installation complete. Open SwiftBar preferences and enable the SwiftBar for Zapier: Updater plugin to activate the plugin."
+echo "Installation complete. Starting SwiftBar..."
+open -a SwiftBar
+echo "Done. You should now see swiftbar-zapier in your MacOS menu bar."
+echo "If not, please ensure your SwiftBar-Plugins folder is set to ~/Applications/SwiftBar-Plugins"
