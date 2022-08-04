071472f0b056ab10b989db809823288beb2aa63b
cwernert <55778578+cwernert@users.noreply.github.com>
2022-08-04 13:49:03 +1000

    Update swiftbar-zapier-updater.1d.sh

--- swiftbar-zapier-updater.1d.sh
+++ swiftbar-zapier-updater.1d.sh
@@ -1,4 +1,17 @@
 #!/usr/bin/env bash
+# <bitbar.title>SwiftBar for Zapier: Updater</bitbar.title>
+# <bitbar.version>v0.0.1</bitbar.version>
+# <bitbar.author>Christian Wernert</bitbar.author>
+# <bitbar.author.github>cwernert</bitbar.author.github>
+# <bitbar.dependencies>javascript, node (fetch, fs)</bitbar.dependencies>
+# <bitbar.desc>Displays contents set by a Zapier channel</bitbar.desc>
+# <bitbar.abouturl>https://github.com/cwernert/swiftbar-zapier</bitbar.abouturl>
+# <swiftbar.hideAbout>false</swiftbar.hideAbout>
+# <swiftbar.hideRunInTerminal>true</swiftbar.hideRunInTerminal>
+# <swiftbar.hideLastUpdated>true</swiftbar.hideLastUpdated>
+# <swiftbar.hideDisablePlugin>true</swiftbar.hideDisablePlugin>
+# <swiftbar.hideSwiftBar>false</swiftbar.hideSwiftBar>
+
 
 # <xbar.title>SwiftBar for Zapier: Updater</xbar.title>
 # <xbar.version>v0.1</xbar.version>
@@ -14,10 +27,22 @@
 # <swiftbar.hideDisablePlugin>true</swiftbar.hideDisablePlugin>
 # <swiftbar.hideSwiftBar>true</swiftbar.hideSwiftBar>
 
+# I think this is likely to allow SwiftBar to parse unwanted files as plugins
+# Probably need to check version numbers, & if GitHub has a later version then invoke a separate shell script which isn't a SwiftBar plugin
+# That way, it can pkill -x SwiftBar, curl the files, chflags hide them all, then open -a SwiftBar again
+
 cd $SWIFTBAR_PLUGINS_PATH
-mkdir swiftbar-zapier
-cd swiftbar-zapier
 curl -O https://raw.githubusercontent.com/cwernert/swiftbar-zapier/main/swiftbar-zapier.10s.js
+curl -O https://raw.githubusercontent.com/cwernert/swiftbar-zapier/main/swiftbar-zapier-updater.1d.sh
 curl -O https://raw.githubusercontent.com/cwernert/swiftbar-zapier/main/package.json
 npm update
-chflags hidden * && chflags nohidden swiftbar-zapier.10s.js
+cd config
+curl -O https://raw.githubusercontent.com/cwernert/swiftbar-zapier/main/swiftbar-zapier-config.sh
+curl -O https://raw.githubusercontent.com/cwernert/swiftbar-zapier/main/swiftbar-zapier-config.js
+curl -O https://raw.githubusercontent.com/cwernert/swiftbar-zapier/main/swiftbar-zapier-config.json
+curl -O https://raw.githubusercontent.com/cwernert/swiftbar-zapier/main/package.json
+chmod 755 swiftbar-zapier-config.js
+chmod 755 swiftbar-zapier-config.sh
+npm update
+cd ../
+chflags hidden * && chflags nohidden swiftbar-zapier.10s.js && chflags nohidden swiftbar-zapier-updater.1d.js
