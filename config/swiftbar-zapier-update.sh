a98da3fe52bcd36ab4d550f00c648bf90477db73
cwernert <55778578+cwernert@users.noreply.github.com>
2022-08-05 08:21:01 +1000

    Add files via upload

---
+++ config/swiftbar-zapier-update.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+pkill -x SwiftBar
+cd $1/swiftbar-zapier
+curl -O https://raw.githubusercontent.com/cwernert/swiftbar-zapier/main/swiftbar-zapier.10s.js
+curl -O https://raw.githubusercontent.com/cwernert/swiftbar-zapier/main/package.json
+npm update
+cd config
+curl -O https://raw.githubusercontent.com/cwernert/swiftbar-zapier/main/swiftbar-zapier-update.sh
+curl -O https://raw.githubusercontent.com/cwernert/swiftbar-zapier/main/swiftbar-zapier-config.sh
+curl -O https://raw.githubusercontent.com/cwernert/swiftbar-zapier/main/swiftbar-zapier-config.js
+curl -O https://raw.githubusercontent.com/cwernert/swiftbar-zapier/main/swiftbar-zapier-config.json
+curl -O https://raw.githubusercontent.com/cwernert/swiftbar-zapier/main/package.json
+chmod 755 swiftbar-zapier-config.js
+chmod 755 swiftbar-zapier-config.sh
+chmod 755 swiftbar-zapier-update.sh
+npm update
+cd $1/swiftbar-zapier
+chflags hidden * && chflags nohidden swiftbar-zapier.10s.js
+open -a SwiftBar
