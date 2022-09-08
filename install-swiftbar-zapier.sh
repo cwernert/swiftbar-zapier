9b2a2e65cde4c68843b720a7b02d699717dc8888
cwernert <55778578+cwernert@users.noreply.github.com>
2022-09-09 09:01:59 +1000

    Adding Node version check (v14.17.0) & colours
    Installer now exits if user has < Node v14.17.0
    Messages echoed to user have some colour to help make the important ones stand out

--- install-swiftbar-zapier.sh
+++ install-swiftbar-zapier.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 clear
+#declaring some ANSI colours to make critical messages stand out
+RED='\033[0;31m'
+GRN='\033[0;32m'
+NC='\033[0m'
+#starting the installation
 echo "This script will install swiftbar-zapier on your computer."
 echo "If SwiftBar is currently running, it will be closed now and opened again once installation is complete."
 while true; do
@@ -7,7 +12,7 @@ read -p "Are you ready to continue? (y/n)" yn
 case $yn in 
   [yY] ) echo "Installing swiftbar-zapier...";
     break;;
-  [nN] ) echo "Exiting. Installation aborted.";
+  [nN] ) echo "Installation aborted; exiting.";
     exit;;
   * ) echo "Invalid response. Please enter Y or N.";;
 esac
@@ -19,7 +24,14 @@ required="Required:"
 reqCount=0
 #checking node
 if which node > /dev/null; then
-  installed=$installed" node"
+  read nodeversion _ <<< $(node -v)
+  nodeversion="${nodeversion:1}"
+  if [[ $nodeversion > 14.16 ]] ; then
+    installed=$installed" node"
+  else
+    echo -e "${RED}Node JS version "$nodeversion" detected.\nPlease update to minimum v14.17.0 to install swiftbar-zapier.\n${NC}Installation aborted; exiting."
+    exit
+  fi
 else
   required=$required" node"
   ((reqCount=reqCount+1))
@@ -61,7 +73,7 @@ if [[ $reqCount > 0 ]] ; then
     case $yn in
       [yY] ) echo "Installing dependencies..."
               break;;
-      [nN] ) echo "Exiting. Installation aborted."
+      [nN] ) echo "Installation aborted; exiting."
               exit;;
       * ) echo "Invalid response. Please enter Y or N.";;
     esac
@@ -72,6 +84,7 @@ if [[ $reqCount > 0 ]] ; then
   fi
   if echo $required | grep -q 'node\|npm'; then
     brew install node
+    #I don't think npm needs to be specifically installed since we require >= Node v14.17
   fi
   if echo $required | grep -q 'swiftbar'; then
     brew install swiftbar
@@ -102,7 +115,7 @@ chmod 755 swiftbar-zapier-update.sh
 npm update
 cd ~/Applications/SwiftBar-Plugins/swiftbar-zapier
 chflags hidden * && chflags nohidden swiftbar-zapier.10s.js
-echo "Installation complete. Starting SwiftBar..."
+echo -e "${GRN}Installation complete. Starting SwiftBar...${NC}"
 open -a SwiftBar
 echo "Done. You should now see swiftbar-zapier in your MacOS menu bar."
 echo "If not, please ensure your SwiftBar-Plugins folder is set to ~/Applications/SwiftBar-Plugins"
